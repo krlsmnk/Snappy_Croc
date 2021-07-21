95c7c8fe0717d58b7aef2b04767258245812f1bf
Karl Smink <krlsmnk@yahoo.com>
2021-07-21 15:25:38 -0500

    Added UE, VR, and parts

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Win64/UE4Editor/Inc/VRExpansionPlugin/VRPlayerController.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VREXPANSIONPLUGIN_VRPlayerController_generated_h
+#error "VRPlayerController.generated.h already included, missing '#pragma once' in VRPlayerController.h"
+#endif
+#define VREXPANSIONPLUGIN_VRPlayerController_generated_h
+
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_SPARSE_DATA
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_RPC_WRAPPERS
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRPlayerController(); \
+	friend struct Z_Construct_UClass_AVRPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AVRPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRPlayerController)
+
+
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRPlayerController(); \
+	friend struct Z_Construct_UClass_AVRPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AVRPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AVRPlayerController)
+
+
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRPlayerController(AVRPlayerController&&); \
+	NO_API AVRPlayerController(const AVRPlayerController&); \
+public:
+
+
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRPlayerController(AVRPlayerController&&); \
+	NO_API AVRPlayerController(const AVRPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRPlayerController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRPlayerController)
+
+
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_PRIVATE_PROPERTY_OFFSET
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_11_PROLOG
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_PRIVATE_PROPERTY_OFFSET \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_SPARSE_DATA \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_RPC_WRAPPERS \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_INCLASS \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_PRIVATE_PROPERTY_OFFSET \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_SPARSE_DATA \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_INCLASS_NO_PURE_DECLS \
+	Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VREXPANSIONPLUGIN_API UClass* StaticClass<class AVRPlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Smacky_Croc_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_VRPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
